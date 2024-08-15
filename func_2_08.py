d4c83b35a0a14b74431c53dc7f74c8c0ccb73163
Турлай Дмитрий Васильевич <C:\Users\\AppData\Roaming\The Bat!>
2024-08-15 08:33:28 +0300

    Stepik function_8 in progress. Add function

--- func_2_08.py
+++ func_2_08.py
@@ -1,3 +1,5 @@
+import re
+
 mails = []
 n = int(input())
 for i in range(n):
@@ -7,28 +9,33 @@ m = int(input())
 res = []
 for i in range(m):
     new_personal.append(input())
-#    count = 0
-#    for item in mails:
-#        if new_personal[i] in item:
-#            count += 1
-#    if count > 0:
-#        res.append(new_personal[i] + str(count) + '@beegeek.bzz')
-#    elif count == 0:
-#        res.append(new_personal[i] + '@beegeek.bzz')
+
+def split_string_and_extract_number(string):
+    # Используем регулярное выражение для разделения строки на текстовую часть и число
+    pattern = r"(\D+)(\d+)"
+    match = re.match(pattern, string)
+    if match:
+        text_part = match.group(1).strip()
+        number_part = int(match.group(2))
+        return text_part, number_part
+    else:
+        return string, None
+
+for i in range(n):
+    text, number = split_string_and_extract_number(mails[i])
+
 for i in range(m):
     for j in range(n):
         count = 0
-        if new_personal[i] == mails[j]:
+        if new_personal[i] == mails[j] or new_personal[i].startswith(mails[j]):
             count += 1
-        elif new_personal[i].startswith(mails[j]):
-            res.append(new_personal[i] + '@beegeek.bzz')
+        elif new_personal[i] != mails[j] and new_personal[i].startswith(mails[j]):
+            if mails[j][-1] != 1:
+
+    if count == 0:
+        res.append(new_personal[i] + '@beegeek.bzz')
+    elif count > 0:
+
 #    mails.append(new_personal[i])
 for i in range(m):
     print(res[i])
-
-#def is_prefix(string, prefix):
-#    return string == prefix or string.startswith(prefix)
-
-#print(is_prefix("hello world", "hello"))  # True
-#print(is_prefix("hello world", "world"))  # False
-#print(is_prefix("hello", "hello"))  # True
