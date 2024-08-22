6d3a33578a0f6afe5927fef376dbebdb23348e41
Турлай Дмитрий Васильевич <C:\Users\\AppData\Roaming\The Bat!>
2024-08-22 17:16:12 +0300

    Stepik function_8 in progress. Change tests code

--- func_2_08.py
+++ func_2_08.py
@@ -1,23 +1,21 @@
 import re
 
-# mails = ['anri-tabuev1', 'arthur-kharisov3']
-# n=2
-#
-# new_personal = ['anri-tabuev', 'anri-tabuev', 'arthur-kharisov', 'arthur-kharisov',
-#                 'arthur-kharisov', 'arthur-kharisov', 'arthur-kharisov', 'arthur-kharisov',
-#                 'arthur-kharisov', 'arthur-kharisov', 'arthur-kharisov', 'arthur-kharisov',
-#                 'arthur-kharisov', 'arthur-kharisov', 'arthur-kharisov']
-# m=15
-# res = []
+mails = ['ivan-petrov', 'petr-ivanov', 'ivan-petrov1',
+         'ivan-ivanov', 'ivan-ivanov1', 'ivan-ivanov2']
+n=6
 
-mails = []
-n = int(input())
-for i in range(n):
-    mails.append(input().split('@')[0])
-new_personal = []
-m = int(input())
-for i in range(m):
-    new_personal.append(input())
+new_personal = ['ivan-ivanov', 'petr-petrov', 'petr-ivanov']
+m=3
+res = []
+
+#mails = []
+#n = int(input())
+#for i in range(n):
+#    mails.append(input().split('@')[0])
+#new_personal = []
+#m = int(input())
+#for i in range(m):
+#    new_personal.append(input())
 res = []
 def split_string_and_extract_number(string):
     # Используем регулярное выражение для разделения строки на текстовую часть и число
@@ -28,22 +26,17 @@ def split_string_and_extract_number(string):
         number_part = int(match.group(2))
         return text_part, number_part
     else:
-        return string, None
+        return string, 0
 
-for i in range(n):
-    text, number = split_string_and_extract_number(mails[i])
-    count = 0
-    for j in range(m):
-        if text == new_personal[j]:
-            count += 1
-            if count == 1 and number != None:
-                res.append(new_personal[j] + str(count))
-            else:
-                if count != number:
-                    res.append(new_personal[j] + str(count))
-                elif count == number:
-                    res.append(new_personal[j] + str(count+1))
-                    count += 1
+def chek_number(n, mas_num):
+    for i in range(1, n + 2):
+        if i not in mas_num:
+            return i
 
+mas_num = []
 for i in range(m):
-    print(res[i] + '@beegeek.bzz')
+    new_personal[i]
+
+#for i in range(m):
+#    print(res[i] + '@beegeek.bzz')
+
