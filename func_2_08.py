d81a18c6de91db893a6e9879d877ffe77342cef4
Турлай Дмитрий Васильевич <C:\Users\\AppData\Roaming\The Bat!>
2024-08-27 11:55:17 +0300

    Stepik function_8 is done

--- func_2_08.py
+++ func_2_08.py
@@ -1,22 +1,13 @@
 import re
 
-mails = ['ivan-petrov', 'petr-ivanov', 'ivan-petrov1',
-         'ivan-ivanov', 'ivan-ivanov1', 'ivan-ivanov2']
-n=6
-
-new_personal = ['ivan-ivanov', 'petr-petrov', 'petr-ivanov']
-m=3
-res = []
-
-#mails = []
-#n = int(input())
-#for i in range(n):
-#    mails.append(input().split('@')[0])
-#new_personal = []
-#m = int(input())
-#for i in range(m):
-#    new_personal.append(input())
-res = []
+mails = []
+n = int(input())
+for i in range(n):
+    mails.append(input().split('@')[0])
+new_personal = []
+m = int(input())
+for i in range(m):
+    new_personal.append(input())
 def split_string_and_extract_number(string):
     # Используем регулярное выражение для разделения строки на текстовую часть и число
     pattern = r"(\D+)(\d+)"
@@ -32,6 +23,27 @@ def chek_number(m, mas_num):
     for i in range(1, m + 2):
         if i not in mas_num:
             return i
+def find_and_add_missing_number(d, key):
+    if key in d:
+        # Получаем список значений, связанных с ключом
+        values = d[key]
+
+        # Создаем список из чисел от 1 до максимального значения в списке + 1
+        all_numbers = list(range(0, max(values) + 2))
+
+        # Находим разницу между всеми числами и значениями в списке
+        missing_number = next((n for n in all_numbers if n not in values), None)
+
+        if missing_number is not None:
+            # Добавляем недостающее число в список
+            d[key].append(missing_number)
+            return missing_number
+        else:
+            return None
+    else:
+        # Если ключа нет, создаем новую пару ключ-значение
+        d[key] = [0]
+        return 0
 
 slov_num = {}
 for i in range(n):
@@ -40,23 +52,11 @@ for i in range(n):
         slov_num[text].extend([number])
     else:
         slov_num[text] = [number]
-    print(slov_num)
 
-
-
-
-
-mas_num = []
 for i in range(m):
-    name = new_personal[i]
-    for j in range(n):
-        text, number = split_string_and_extract_number(mails[j])
-        if name == text:
-            mas_num.append(number)
-    num = chek_number(m, mas_num)
-    new_personal[i] = new_personal[i] + str(num)
-    mas_num.append(num)
+    n = find_and_add_missing_number(slov_num, new_personal[i])
+    if n > 0:
+        new_personal[i] = new_personal[i] + str(n)
 
-#for i in range(m):
-#    print(res[i] + '@beegeek.bzz')
-
+for i in range(m):
+    print(new_personal[i] + '@beegeek.bzz')
