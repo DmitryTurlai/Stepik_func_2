d3918b3f1cc804bceacf42b55766072c0838f2ca
Турлай Дмитрий Васильевич <C:\Users\\AppData\Roaming\The Bat!>
2024-10-15 08:29:34 +0300

    Stepik code is_avalible_date is done

--- is_available_date.py
+++ is_available_date.py
@@ -18,8 +18,27 @@ def is_available_date(booked_dates, date_for_booking):
             while dt <= end_date:
                 res.append(dt)
                 dt += timedelta(days=1)
+    date_for_live = []
+    try:
+        start_str, end_str = date_for_booking.split('-')
+        start_date = datetime.strptime(start_str, '%d.%m.%Y').date()
+        end_date = datetime.strptime(end_str, '%d.%m.%Y').date()
+    except ValueError:
+        start_date = end_date = datetime.strptime(date_for_booking, '%d.%m.%Y').date()
 
-    return res
+    if start_date == end_date:
+        date_for_live.append(start_date)
+    else:
+        dt = start_date
+        while dt <= end_date:
+            date_for_live.append(dt)
+            dt += timedelta(days=1)
 
+    common_values = True
+    for value in date_for_live:
+        if value in res:
+            common_values = False
+            break
+    return common_values
 result = is_available_date(['04.11.2021', '05.11.2021-09.11.2021'], '01.11.2021')
 print(result)
